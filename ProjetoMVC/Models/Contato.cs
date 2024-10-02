0a1853d557c51db1905e1b3d15ae8d7851561fe9
Bruno Henrique <bruno_hcs2009@hotmail.com>
2024-10-02 09:51:46 -0300

    criacao do projeto

---
+++ ProjetoMVC/Models/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoMVC.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O número de telefone é obrigatório.")]
+        [RegularExpression(@"\(\d{2}\) \d{5}-\d{4}", ErrorMessage = "O telefone deve estar no formato (XX) XXXXX-XXXX.")]
+        public string Telefone { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
